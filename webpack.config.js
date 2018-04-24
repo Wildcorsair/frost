8bc81e04b3cdc0cd4631c4b5ab926647e223a1da
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-04-24 22:42:20 +0300

    Added webpack-copy-plugin and settings for using it

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const options = {
+  debug: 'info',
+  copyUnmodified: true
+};
 
 module.exports = {
   mode: 'development',
@@ -9,27 +15,32 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        exclude: /\/node_modules\//,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'react']
-        }
+      test: /\.js$/,
+      exclude: /\/node_modules\//,
+      loader: 'babel-loader',
+      query: {
+        presets: ['env', 'react']
+      }
     }, {
-        test: /\.css$/,
-        exclude: /\/node_modules\//,
-        loader: ['style-loader', 'css-loader'],
+      test: /\.css$/,
+      exclude: /\/node_modules\//,
+      loader: ['style-loader', 'css-loader'],
     }, {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        exclude: /node_modules/,
-        loader: 'url-loader?limit=1024&name=/public/dist/fonts/[name].[ext]'
+      test: /\.(woff|woff2|eot|ttf|svg)$/,
+      exclude: /node_modules/,
+      loader: 'url-loader?limit=1024&name=/public/dist/fonts/[name].[ext]'
     }, {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
-        options: {
-            name: '/src/fonts/[name].[ext]',
-            outputPath: '/public/dist'
-        }
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+          name: '/src/fonts/[name].[ext]',
+          outputPath: '/public/dist'
+      }
     }]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/fonts/**/*', to: 'fonts/', flatten: true }
+    ], options)
+  ]
 };
