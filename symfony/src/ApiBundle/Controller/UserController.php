1e2403cce56bb989a16214445dff850ed5c9fdd0
Vladimir Zakharchenko <sirius.flashlight@gmail.com>
2018-04-24 17:28:45 +0300

    Added User Controller

--- symfony/src/ApiBundle/Controller/UserController.php
+++ symfony/src/ApiBundle/Controller/UserController.php
@@ -6,17 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class UserController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/login");
      */
-    public function indexAction()
-    {
+    public function login() {
         return new Response(
-          'Frost API v.1.0',
+          json_encode(array(
+              'status' => 'ok',
+              '_token' => 'fe524rwe64ERgf54%$da63dFKJ654kUW5ds4f6asd'
+          )),
           Response::HTTP_OK,
-          array('Content-Type' => 'text/html')
+          array('Content-Type' => 'application/json')
         );
     }
 }
